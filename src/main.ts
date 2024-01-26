e3a11a599ad6a176467b666aa3d4fc0c6a966fb7
Gabriel Pretel <gabrilinex@gmail.com>
2024-01-26 12:39:11 +0100

    Cambiados cast por if y reorganizado el código para mejor legilibilidad

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,94 @@
 let turno: number = 1;
-
-const numeroPantalla = document.getElementById("numero-turno") as HTMLHeadingElement;
+const numeroPantalla = document.getElementById("numero-turno");
+const botonSiguiente = document.getElementById("siguiente");
+const botonAnterior = document.getElementById("anterior");
+const botonReset = document.getElementById("reset");
+const valorInput = document.getElementById("personalizado");
+const botonCambiar = document.getElementById("boton-cambiar");
 
 function sumaTurno(): void {
   turno++;
-  numeroPantalla.innerHTML = turno.toString().padStart(2, "0");
+  if (
+    numeroPantalla !== null &&
+    numeroPantalla !== undefined &&
+    numeroPantalla instanceof HTMLHeadingElement
+  ) {
+    numeroPantalla.innerHTML = turno.toString().padStart(2, "0");
+  }
 }
-const botonSiguiente = document.getElementById(
-  "siguiente"
-) as HTMLButtonElement;
-
-botonSiguiente.addEventListener("click", () => sumaTurno());
 
 function restaTurno(): void {
   if (turno > 1) {
     turno--;
   }
-  numeroPantalla.innerHTML = turno.toString().padStart(2, "0");
+  if (
+    numeroPantalla !== null &&
+    numeroPantalla !== undefined &&
+    numeroPantalla instanceof HTMLHeadingElement
+  ) {
+    numeroPantalla.innerHTML = turno.toString().padStart(2, "0");
+  }
 }
-const botonAnterior = document.getElementById("anterior") as HTMLButtonElement;
-
-botonAnterior.addEventListener("click", () => restaTurno());
 
 function reset() {
   turno = 0;
-  numeroPantalla.innerHTML = turno.toString().padStart(2, "0");
+  if (
+    numeroPantalla !== null &&
+    numeroPantalla !== undefined &&
+    numeroPantalla instanceof HTMLHeadingElement
+  ) {
+    numeroPantalla.innerHTML = turno.toString().padStart(2, "0");
+  }
 }
-
-const botonReset = document.getElementById("reset") as HTMLButtonElement;
-botonReset.addEventListener("click", () => reset());
 
 // Botón para que el operario pueda cambiar el turno a uno en concreto
 
-const valorInput = document.getElementById("personalizado") as HTMLInputElement;
-
 function cambiarNumero(): void {
-  turno = parseInt(valorInput.value);
-  numeroPantalla.innerHTML = turno.toString().padStart(2, "0");
+  if (
+    valorInput !== null &&
+    valorInput !== undefined &&
+    valorInput instanceof HTMLInputElement
+  ) {
+    turno = parseInt(valorInput.value);
+  }
+
+  if (
+    numeroPantalla !== null &&
+    numeroPantalla !== undefined &&
+    numeroPantalla instanceof HTMLHeadingElement
+  ) {
+    numeroPantalla.innerHTML = turno.toString().padStart(2, "0");
+  }
 }
 
-const botonCambiar = document.getElementById(
-  "boton-cambiar"
-) as HTMLButtonElement;
-botonCambiar.addEventListener("click", () => cambiarNumero());
+if (
+  botonSiguiente !== null &&
+  botonSiguiente !== undefined &&
+  botonSiguiente instanceof HTMLButtonElement
+) {
+  botonSiguiente.addEventListener("click", () => sumaTurno());
+}
+
+if (
+  botonAnterior !== null &&
+  botonAnterior !== undefined &&
+  botonAnterior instanceof HTMLButtonElement
+) {
+  botonAnterior.addEventListener("click", () => restaTurno());
+}
+
+if (
+  botonReset !== null &&
+  botonReset !== undefined &&
+  botonReset instanceof HTMLButtonElement
+) {
+  botonReset.addEventListener("click", () => reset());
+}
+
+if (
+  botonCambiar !== null &&
+  botonCambiar !== undefined &&
+  botonCambiar instanceof HTMLButtonElement
+) {
+  botonCambiar.addEventListener("click", () => cambiarNumero());
+}
